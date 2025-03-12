0c56685d7fde5ad5568caeee207914950d8dfffc
AADHIISHVAR <aadhiishvark7484@gmail.com>
2025-03-13 02:02:47 +0530

    Update gpuTest.py

--- gpuTest.py
+++ gpuTest.py
@@ -12,7 +12,16 @@ print("Transformers version:", transformers.__version__)
 print("Pillow version:", Image.__version__)
 print("IPython version:", IPython.__version__)
 print("CUDA Available:", torch.cuda.is_available())
-print("___________________________________________________________________________________________________________________")
+print("_______________________________________________________________________________________________")
+
+# Attempt to load the local CLIP model
+try:
+    model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32", cache_dir="C:\\Users\\Admin\\clip_model")
+    processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32", cache_dir="C:\\Users\\Admin\\clip_model")
+    print("CLIP model and processor loaded successfully.")
+except Exception as e:
+    print(f"Error loading CLIP model: {e}")
+    
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
@@ -21,15 +30,5 @@ print('Sample programn to check cuda is workin perfect')
 x = torch.rand(3, 3).to(device)
 print(x)
 
-
-#Attempt to load the local CLIP model
-try:
-    print("loding modal")
-    # model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32", cache_dir="C:/Users/abhej/.cache/huggingface/models/models--openai--clip-vit-base-patch32")
-    # processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32", cache_dir="C:/Users/abhej/.cache/huggingface/models/models--openai--clip-vit-base-patch32")
-    model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32", cache_dir="C:/Users/abhej/.cache/huggingface/models/models--openai--clip-vit-base-patch32")
-    processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32", cache_dir="C:/Users/abhej/.cache/huggingface/models/models--openai--clip-vit-base-patch32")
-
-    print("CLIP model and processor loaded successfully.")
-except Exception as e:
-    print(f"Error loading CLIP model: {e}")
+#model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32", cache_dir="C:\\Users\\Admin\\.cache\\huggingface\\models")
+#processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32", cache_dir="C:\\Users\\Admin\\.cache\\huggingface\\models")
